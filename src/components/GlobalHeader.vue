1865478b0b0c2ea1f057b371307f93e6252b734c
増井高 <>
2020-08-28 15:26:53 +0900

    init

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <!-- ここにHeaderを自作してみる-->
+        <!--参考サイト:http://www.tohoho-web.com/bootstrap/index.html-->
     </div>
 </template>
 <script>
---
+++ src/components/NextPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>
+            こんにちは!!
+        </h1>
+        <div class="Container">
+            <!--リストをここに作成する-->
+            <!--参考サイト:http://www.tohoho-web.com/bootstrap/index.html-->
+        </div>
+    </div>
+</template>
